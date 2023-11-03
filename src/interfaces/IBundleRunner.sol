ada9a868c1dbe0fa20b6a666b372eae38c05b546
Ian Guimaraes <ianguimaraes31@gmail.com>
2023-11-03 17:23:07 -0300

    feat: bundle runner

--- src/interfaces/IBundleRunner.sol
+++ src/interfaces/IBundleRunner.sol
@@ -15,17 +15,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
-import {IBundleRunner} from './interfaces/IBundleRunner.sol';
-import {IBundler} from './interfaces/IBundler.sol';
-
-contract BundleRunner is IBundleRunner, Ownable {
-    constructor(address _owner) Ownable(_owner) {}
-
-    function runUserBundles(address[] calldata _bundlers) external onlyOwner {
-        for (uint8 i = 0; i < _bundlers.length; i++) {
-            IBundler(_bundlers[i]).runBundle();
-        }
+interface IBundleRunner {
+    struct BundleExecution {
+        address user;
+        address[] bundleIds;
     }
-}
 
+    function runUserBundles(BundleExecution[] calldata _bundleExecutions) external;
+}
